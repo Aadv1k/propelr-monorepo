71bbc235efdf4f612b2096fa2d2551dc1c664817
Aadv1k <aadv1k@outlook.com>
2023-04-22 15:36:12 +0530

    Add JWT common module

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -2,10 +2,12 @@ import * as http from "./http";
 import validateEmail from "./emailValidator";
 import bloomTable from "./bloomTable";
 import validateSchema from "./schemaValidator";
+import * as jwt from "./jwt";
 
 export {
   http,
   validateEmail,
   validateSchema,
   bloomTable,
+  jwt,
 }
---
+++ packages/common/jwt.ts
@@ -0,0 +1,48 @@
+import crypto from "node:crypto";
+
+function hmac256(txt: string, secret: string): string {
+  return crypto.createHmac("sha256", secret).update(txt).digest("hex")
+}  
+
+const HEADER = {
+  alg: "HS256",
+  typ: "JWT"
+}
+
+function b64Encode(text: string): string {
+  return Buffer.from(text).toString("base64")
+    .replace('+', '-')
+    .replace('/', '_')
+    .replace(/=+$/, '');
+}
+
+function b64Decode(text: string): string {
+  return Buffer.from(text, "base64").toString()
+}
+
+
+export function sign(obj: any, secret: string) {
+  const head = b64Encode(JSON.stringify(HEADER));
+  const payload = b64Encode(JSON.stringify(obj));
+  const tail = hmac256(`${head}.${payload}`, secret);
+
+  return `${head}.${payload}.${tail}`;
+}
+
+export function verify(token: string, secret: string): boolean {
+  let split = token.split(".");
+
+  if (split.length !== 3) return false;
+
+  let head = b64Decode(split[0]);
+  let payload = b64Decode(split[1]);
+  let tail = split[2];
+
+  if (JSON.parse(head).alg !== HEADER.alg) return false;
+
+  const decoded = hmac256(`${b64Encode(head)}.${b64Encode(payload)}`, secret);
+
+  if (decoded !== tail) return false;
+
+  return true;
+}
