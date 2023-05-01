454d56f9dce27d178a795e76cbe7efd572d2e41e
Aadv1k <aadv1k@outlook.com>
2023-05-01 08:45:13 +0530

    Add shared frontend and backend globals

--- packages/common/schemaValidator.ts
+++
@@ -1,8 +0,0 @@
-export default function (obj: any, schema: any): boolean {
-  for (const key in obj) {
-    if (!schema[key] || typeof obj[key] !== schema[key]) {
-      return false;
-    }
-  }
-  return true;
-}
